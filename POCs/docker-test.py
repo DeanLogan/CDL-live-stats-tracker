93a41cb7b1125715fd3aecbbd398b64f20a8c68b
Dean Logan <deanlogan42@gmail.com>
2024-03-30 19:45:05 +0000

    Started threading tests for capture images threads with reading different roi's

--- POCs/docker-test.py
+++ POCs/docker-test.py
@@ -17,8 +17,6 @@ from selenium.webdriver.common.keys import Keys
 
 # the killfeed will not only shows the players name but also the team within a clan tag that typically looks like this [TOR] Scrap, however, the OCR is not able to read the brackets that well and often confuses them wiht I so for the dictionsaries I have hard coded the names with i instead of the brackers (i in lowercase because within the similar function I set what the OCR reads to all lowercase).
 
-driver = None
-
 kills = {
     "scrap": 2,
     "cleanx": 6,
@@ -41,48 +39,65 @@ deaths = {
     "breszy": 3
 }
 
+def setup_browser():
+    print("opening browser")
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox()
+    driver.install_addon('uBlock0_1.56.1rc5.firefox.signed.xpi', temporary=True) # adding ublock ad blocker
+    url = "https://www.youtube.com/watch?v=FjclYlb8dRY&list=WL&index=64&t=30s"
+    driver.get(url)
+    print("video loaded")
+    time.sleep(3) # Wait for the video to load
 
-def capture_images(q):
+    # click youtubes cookie agreement
+    print("clicking cookie agreement")
+    button = driver.find_element(by=By.XPATH, value='/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button')
+    button.click()
+
+    time.sleep(5) # Wait for agreement to be rejected and then click the play button
+
+    # plays yt video as autoplay is off
+    print("playing video")
+    button = driver.find_element(by=By.XPATH, value='/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[6]/button')
+    button.click()
+
+    actions = ActionChains(driver)
+    actions.send_keys('m').perform() # press the m key to mute the video
+    actions.send_keys('f').perform() # press the f key to go full screen
+
+    time.sleep(0.5) # Wait for full screen animation
+
+    return driver
+
+def capture_images(q, bbox, driver):
     print("starting capture images")
-    # bounding box (x1, y1, x2, y2)
-    x1, y1, x2, y2 = 144, 616, 350, 635
 
     while True:
         screenshot = driver.get_screenshot_as_png()
         image = Image.open(io.BytesIO(screenshot))
-        # roi = image.crop((x1, y1, x2, y2))
-        # frame = np.array(roi)
-        # image.show()
-        frame = np.array(image)
+        roi = image.crop(bbox)
+        frame = np.array(roi)
         result = pytesseract.image_to_string(frame, lang='eng')
-        print(result)
         if len(result) > 6:
             q.put(result)
-        time.sleep(0.1)
 
 
 def process_images(q):
-    players=["itoriscrap", "itoricleanx", "itoriinsight", "itorienvoy", "iseaihuke", "iseaiabuzah", "iseaiarcitys", "iseaibreszy"]
     prev_kill = ""
     while True:
         result = q.get()
-        print(result)
         players = identify_names(result)
         if len(players) > 1:
             current_kill = players[0] + " " + players[1]
             if current_kill != prev_kill:
-                print("------------------------------------")
                 kill(current_kill)
-                print("====================================")
                 prev_kill = current_kill
                 print(current_kill)
-                kd()
-                print("------------------------------------")
 
 # this function isn't great maybe another way of identifying the names would be better
 def identify_names(input_string):
     identified_names = []
-    scores = []
 
     words = input_string.lower().split()
 
@@ -91,7 +106,7 @@ def identify_names(input_string):
         max_name = None
         for name in kills:
             score = fuzz.ratio(word, name)
-            if score > 55 and score > max_score:
+            if score > 60 and score > max_score:
                 max_score = score
                 max_name = name
         if max_name is not None:
@@ -123,12 +138,32 @@ def kd():
 
 def testing_selecting_text():
     # bounding box (x1, y1, x2, y2)
-    x1, y1, x2, y2 = 144, 616, 350, 635
+    x1, y1, x2, y2 = 44, 640, 365, 667
+
     screenshot = driver.get_screenshot_as_png()
     image = Image.open(io.BytesIO(screenshot))
     roi = image.crop((x1, y1, x2, y2))
     frame = np.array(roi)
-    image.show()
+    roi.show()
+
+    screenshot = driver.get_screenshot_as_png()
+    image = Image.open(io.BytesIO(screenshot))
+    roi = image.crop((x1, y1, x2, y2))
+    frame = np.array(roi)
+    roi.show()
+
+    screenshot = driver.get_screenshot_as_png()
+    image = Image.open(io.BytesIO(screenshot))
+    roi = image.crop((x1, y1, x2, y2))
+    frame = np.array(roi)
+    roi.show()
+
+    screenshot = driver.get_screenshot_as_png()
+    image = Image.open(io.BytesIO(screenshot))
+    roi = image.crop((x1, y1, x2, y2))
+    frame = np.array(roi)
+    roi.show()
+    
     result = pytesseract.image_to_string(frame, lang='eng')
     print(result)
 
@@ -136,45 +171,16 @@ if __name__ == "__main__":
     print("starting program")
     q = queue.Queue()
 
-    print("capturing images starting")
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox()
-    driver.install_addon('uBlock0_1.56.1rc5.firefox.signed.xpi', temporary=True) # adding ublock ad blocker
-    url = "https://www.youtube.com/watch?v=FjclYlb8dRY&list=WL&index=64"
-    driver.get(url)
-    print("video loaded")
-    time.sleep(3) # Wait for the video to load
+    driver = setup_browser()
 
-    # click youtubes cookie agreement
-    print("clicking cookie agreement")
-    button = driver.find_element(by=By.XPATH, value='/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button')
-    button.click()
+    capture_thread_1 = threading.Thread(target=capture_images, args=(q,(44, 640, 365, 667), driver,))
+    capture_thread_2 = threading.Thread(target=capture_images, args=(q,(44, 613, 365, 640), driver,))
+    process_thread = threading.Thread(target=process_images, args=(q,))
 
-    time.sleep(2) # Wait for agreement to be rejected and then click the play button
+    capture_thread_1.start()
+    capture_thread_2.start()
+    process_thread.start()
 
-    # plays yt video as autoplay is off
-    print("playing video")
-    button = driver.find_element(by=By.XPATH, value='/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[6]/button')
-    button.click()
-
-    time.sleep(1) # Wait for the video to start playing
-
-    # press the f q to go full screen
-    actions = ActionChains(driver)
-    actions.send_keys('f').perform()
-
-    time.sleep(0.5) # Wait for full screen animation
-
-    testing_selecting_text()
-
-    # capture_thread_1 = threading.Thread(target=capture_images, args=(q,))
-    # capture_thread_2 = threading.Thread(target=capture_images, args=(q,))
-    # process_thread = threading.Thread(target=process_images, args=(q,))
-    # capture_thread_1.start()
-    # capture_thread_2.start()
-    # process_thread.start()
-
-    # capture_thread_1.join()
-    # capture_thread_2.join()
-    # process_thread.join()
+    capture_thread_1.join()
+    capture_thread_2.join()
+    process_thread.join()
