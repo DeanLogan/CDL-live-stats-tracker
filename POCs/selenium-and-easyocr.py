b245143d70c4ad4cfc6bc2fb1ee86fc509cbc039
Dean Logan <deanlogan42@gmail.com>
2024-04-03 18:07:13 +0100

    Testing performance of selenium and easyocr together compared to tesseract and selenium

--- POCs/selenium-and-easyocr.py
+++ POCs/selenium-and-easyocr.py
@@ -1,19 +1,15 @@
 import io
 import time
 import queue
+import easyocr
 import sqlite3
-import keyboard
 import threading
-import subprocess
 import numpy as np
-import pytesseract
-from fuzzywuzzy import fuzz
 from selenium import webdriver
 from PIL import ImageGrab, Image
 from difflib import SequenceMatcher
 from expiringdict import ExpiringDict
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
@@ -21,25 +17,25 @@ from selenium.webdriver.common.action_chains import ActionChains
 killfeed = ExpiringDict(max_len=9, max_age_seconds=5.5)
 
 kills = {
-    "[tor]scrap": 2,
-    "[tor]cleanx": 6,
-    "[tor]insight": 2,
-    "[tor]envoy": 2,
-    "[sea]huke": 4,
-    "[sea]abuzah": 2,
-    "[sea]arcitys": 5,
-    "[sea]breszy": 5
+    "itoriscrap": 2,
+    "itoricleanx": 6,
+    "itoriinsight": 2,
+    "itorienvoy": 2,
+    "iseaihuke": 4,
+    "iseaiabuzah": 2,
+    "iseaiarcitys": 5,
+    "iseaibreszy": 5
 }
 
 deaths = {
-    "[tor]scrap": 4,
-    "[tor]cleanx": 3,
-    "[tor]insight": 4,
-    "[tor]envoy": 5,
-    "[sea]huke": 4,
-    "[sea]abuzah": 5,
-    "[sea]arcitys": 0,
-    "[sea]breszy": 3
+    "itoriscrap": 4,
+    "itoricleanx": 3,
+    "itoriinsight": 4,
+    "itorienvoy": 5,
+    "iseaihuke": 4,
+    "iseaiabuzah": 5,
+    "iseaiarcitys": 0,
+    "iseaibreszy": 3
 }
 
 CLAN_TAGS = {
@@ -94,14 +90,6 @@ def get_teams(driver):
             break
     return team1, team2
 
-def get_text(driver, bbox):
-    screenshot = driver.get_screenshot_as_png()
-    image = Image.open(io.BytesIO(screenshot))
-    roi = image.crop(bbox)
-    image = image.convert('L')
-    result = pytesseract.image_to_string(roi, lang='eng')
-    return result
-
 def setup_browser():
     print("opening browser")
     options = Options()
@@ -144,139 +132,82 @@ def setup_browser():
 
     return driver
 
-def capture_images(q, bboxes, driver):
-    print("starting capture images")
+def get_text(reader, bbox):
+    screenshot = driver.get_screenshot_as_png()
+    image = Image.open(io.BytesIO(screenshot))
+    roi = image.crop(bbox)
+    npArray = np.array(roi)
+    result = reader.readtext(npArray)
+    return result
+
+def capture_killfeed(q, bboxes, driver, reader):
+    print("Capture thread started")
     while True:
         screenshot = driver.get_screenshot_as_png()
         image = Image.open(io.BytesIO(screenshot))
         for bbox in bboxes:
             roi = image.crop(bbox)
-            # image = image.convert('L')
-            frame = np.array(roi)
-            result = pytesseract.image_to_string(frame, lang='eng')
-            if len(result) > 6:
+            npArray = np.array(roi)
+            result = reader.readtext(npArray)
+            if len(result) > 1:
                 q.put(result)
 
 
 def process_images(q):
+    print("Process thread started")
+    players=["itoriscrap", "itoricleanx", "itoriinsight", "itorienvoy", "iseaihuke", "iseaiabuzah", "iseaiarcitys", "iseaibreszy"]
     while True:
-        result = q.get()
-        # resultSplit = identify_names(result)
-        # if len(resultSplit) > 1:
-        #     print(resultSplit)
-        #    current_kill = resultSplit[0] + " " + resultSplit[1]
-        resultSplit = result.split("[")
-        if len(resultSplit) > 2:
-            current_kill = similar(resultSplit[1]) + " " + similar(resultSplit[2])
-            if (not killfeed.get(current_kill, False)) and current_kill[0] != "?" and current_kill[len(current_kill)-1] != "?":
-                kill(current_kill)
-                killfeed[current_kill] = True
-                print(current_kill)
-
-# this function isn't great maybe another way of identifying the names would be better
-def identify_names(input_string):
-    identified_names = []
-
-    words = input_string.lower().split()
-
-    for word in words:
-        max_score = 0
-        max_name = None
-        for name in kills:
-            score = fuzz.ratio(word, name)
-            if score > 60 and score > max_score:
-                max_score = score
-                max_name = name
-        if max_name is not None:
-            identified_names.append(max_name)
-
-    return identified_names[:2]
-
-# temp function to test usage of sets and list comprehension
-def identify_names2(input_string):
-    words = input_string.lower().split()
-    kills_set = set(kills)
-
-    identified_names = []
-    for word in words:
-        max_score_name = max((fuzz.ratio(word, name), name) for name in kills_set)
-        if max_score_name[0] > 60:
-            identified_names.append(max_score_name[1])
-
-    return identified_names[:2]
-
+        try:
+            result = q.get()
+            if len(result) > 1:
+                current_kill = similar(players, result[0][1]) + " " + similar(players, result[1][1])
+                if (not killfeed.get(current_kill, False)) and current_kill[0] != "?" and current_kill[len(current_kill)-1] != "?":
+                    killfeed[current_kill] = True
+                    print(current_kill)
+                    kill(current_kill)
+        except:
+            continue
 
 def kill(current_kill):
-    killer = current_kill.split(" ")[0]
-    killed = current_kill.split(" ")[1]
+    current_kill_split = current_kill.split(" ")
+    killer = current_kill_split[0]
+    killed = current_kill_split[1]
     kills[killer] += 1
     deaths[killed] += 1
 
-def similar(b):
+def similar(similar_words, str):
     most_similar = "?"
     highest_score = 0
-    b = b.lower().replace(" ", "")
-    for player in kills:
-        score = SequenceMatcher(None, player, b).ratio()
+    str  = str.lower()
+    for word in similar_words:
+        score = SequenceMatcher(None, word, str).ratio()
         if score > highest_score and score > 0.60:
             highest_score = score
-            most_similar = player
+            most_similar = word
+
     return most_similar
 
 def kd():
     for player in kills:
         print(player + " " + str(kills[player]) + " / " + str(deaths[player]))
 
-def testing_selecting_text():
-    # bounding box (x1, y1, x2, y2)
-    x1, y1, x2, y2 = 44, 640, 365, 667
-
-    screenshot = driver.get_screenshot_as_png()
-    image = Image.open(io.BytesIO(screenshot))
-    roi = image.crop((x1, y1, x2, y2))
-    frame = np.array(roi)
-    roi.show()
-
-    screenshot = driver.get_screenshot_as_png()
-    image = Image.open(io.BytesIO(screenshot))
-    roi = image.crop((x1, y1, x2, y2))
-    frame = np.array(roi)
-    roi.show()
-
-    screenshot = driver.get_screenshot_as_png()
-    image = Image.open(io.BytesIO(screenshot))
-    roi = image.crop((x1, y1, x2, y2))
-    frame = np.array(roi)
-    roi.show()
-
-    screenshot = driver.get_screenshot_as_png()
-    image = Image.open(io.BytesIO(screenshot))
-    roi = image.crop((x1, y1, x2, y2))
-    frame = np.array(roi)
-    roi.show()
-    
-    result = pytesseract.image_to_string(frame, lang='eng')
-    print(result)
-
 if __name__ == "__main__":
-    print("starting program - changes made v3")
+    print("starting selenium and easyocr script")
+    reader = easyocr.Reader(['en'])
     q = queue.Queue()
 
     driver = setup_browser()
 
     bboxes = [(44, 637, 365, 667),(44, 610, 365, 640)]
 
-    capture_thread_1 = threading.Thread(target=capture_images, args=(q,[(44, 637, 365, 667),(44, 610, 365, 640)], driver,))
-    capture_thread_2 = threading.Thread(target=capture_images, args=(q,[(44, 637, 365, 667),(44, 610, 365, 640)], driver,))
+    capture_thread_1 = threading.Thread(target=capture_killfeed, args=(q,bboxes, driver, reader,))
+    capture_thread_2 = threading.Thread(target=capture_killfeed, args=(q,bboxes, driver, reader,))
     process_thread_1 = threading.Thread(target=process_images, args=(q,))
-    process_thread_2 = threading.Thread(target=process_images, args=(q,))
 
     capture_thread_1.start()
     process_thread_1.start()
     capture_thread_2.start()
-    process_thread_2.start()
 
     capture_thread_1.join()
     process_thread_1.join()
     capture_thread_2.join()
-    process_thread_2.join()
