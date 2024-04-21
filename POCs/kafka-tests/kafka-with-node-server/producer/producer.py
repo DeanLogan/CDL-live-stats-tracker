66b9de8172c6fc4a196be0642d3bdda87f449be4
Dean Logan <deanlogan42@gmail.com>
2024-04-21 21:22:32 +0100

    Additional work on trying to get producer container to connect to kafka container

--- POCs/kafka-tests/kafka-with-node-server/producer/producer.py
+++ POCs/kafka-tests/kafka-with-node-server/producer/producer.py
@@ -1,13 +1,13 @@
 import time
 from confluent_kafka import Producer
 
-bootstrap_servers = 'kafka:9092' 
+bootstrap_servers = 'localhost:9092' 
 print(f'Connecting to {bootstrap_servers}')
 
 p = Producer({'bootstrap.servers': bootstrap_servers})
 
 while True:
-    print("what")
     p.produce('test', 'message every 15 seconds')
+    print('message sent')
     p.flush()
     time.sleep(15)
